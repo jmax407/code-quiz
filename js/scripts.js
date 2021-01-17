17bc11c0d371dd3719a2959b76f11aaa43e1d645
Jmax <jmax407@gmail.com>
2021-01-17 17:22:52 -0500

    added start, added questions, added timer

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,99 @@
-var startBtn = document.querySelector(".start-btn");
+var questionCounter = 0;
+var questionContainer = document.querySelector(".question-container");
+var main = document.querySelector("main");
+var score = 0;
+var currentTime = 75;
 
-var questionsArray = [
+
+var questionsObj = [
     {
-        question1Obj:  "this is question 1",
-        answer1: "a",
-        answer2: "b",
-        answer3: "c",
-        answer4: "d",
+        question:  "this is question 1",
+        answersArr: ["a","b","c","d"],
+        correctAnswer: "b"
 
     },
-
     {
-        question1Obj:  "this is question 2",
-        answer1: "a",
-        answer2: "b",
-        answer3: "c",
-        answer4: "d",
+        question:  "this is question 2",
+        answersArr: ["a","b","c","d"],
+        correctAnswer: "b"
 
     },
+]
+
+ var answerBtnHandler = function(event) {
+     var targetEl = event.target;
+    console.log(answerBtnHandler);
+ }
+  
+
+var startQuizHandler = function() {
+    event.preventDefault();
+    document.querySelector(".intro").remove();
+    var createQuiz = function() {
+
+        document.querySelector("#timer").innerHTML = "Time: " + currentTime;
+        var timeLeft = setInterval(function() {
+                currentTime--;
+                document.querySelector("#timer").innerHTML = "Time: " + currentTime;
+                if(currentTime <= 0) {
+                    clearInterval(timeLeft);
+                }
+        } ,1000);
+        
+        
+        while( questionCounter < questionsObj.length) {
     
-    {
-        question1Obj:  "this is question 3",
-        answer1: "a",
-        answer2: "b",
-        answer3: "c",
-        answer4: "d",
-
-    },
+            // for each question object in array create a question item
+            var createQuestion = document.createElement("li");
+            createQuestion.className ="question-item";
+            createQuestion.setAttribute("question-id", questionCounter);
     
-    {
-        question1Obj:  "this is question 4",
-        answer1: "a",
-        answer2: "b",
-        answer3: "c",
-        answer4: "d",
-
+            //create Question
+            var question = document.createElement("h1");
+            question.className = "question-value";
+            question.innerHTML = questionsObj[questionCounter].question; 
+            createQuestion.appendChild(question);
+            
+            // create answer container
+            var answerContainer = document.createElement("div");
+            answerContainer.className = "answers";
+            createQuestion.appendChild(answerContainer);
+    
+            // create a button for each answer
+            for( j = 0; j < questionsObj[questionCounter].answersArr.length; j++) {
+                var answer = document.createElement("button");
+                answer.className = "btn";
+                answer.innerHTML = questionsObj[questionCounter].answersArr[j];
+                answerContainer.appendChild(answer);
+       
+            } // End for loop
+            var correctAnswer = questionsObj[questionCounter].correctAnswer;
+            console.log(correctAnswer[questionCounter]);
+            var userAnswer = document.querySelector
+            if(userAnswer === correctAnswer) {
+                document.querySelector('.checker').innerHTML = "<h2>Correct!</h2>";
+               score = score++;
+            }
+            else {
+                document.querySelector('.checker').innerHTML = "<h2>Wrong!</h2>";
+                currentTime = currentTime - 5;
+           }
+    
+        
+         
+        questionContainer.appendChild(createQuestion);
+        // document.querySelector('.answers').addEventListener("click", answerBtnHandler);
+        questionCounter++;
+        
+        } // End for loop
+        
+    
     }
-]
-var answersArray = [
-    {}
-]
-var beginQuiz = function() {
-    console.log('lets begin');
+    createQuiz();
+    
 }
 
-startBtn.addEventListener("click", beginQuiz);
 
+
+
+document.querySelector("#start-btn").addEventListener("click", startQuizHandler);
